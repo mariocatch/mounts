aad25d92382afb9a51c24576dc1256fa93eb9056
mariocatch <mariocatch@gmail.com>
2015-05-23 12:51:56 -0400

    Add grid layout from ajax results, and design/css related to showing mounts in the grid.

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -22,7 +22,33 @@ $(document).ready(function() {
             dataType: 'jsonp', 
 			jsonp: 'jsonp',
 			success: function(data) {
-				$('#results div').html(JSON.stringify(data));
+				/*
+					<div class="mount">
+						<div class="mount-header">
+							<h3 class="mount-name">Steed</h3>
+						</div>
+						<div class="mount-body">
+							<p>Placeholder</p>
+						</div>
+					</div>
+				*/		
+				
+				var collected = data.mounts.numCollected;
+				var total = data.mounts.numCollected + data.mounts.numNotCollected;
+				var mounts = data.mounts.collected;
+				
+				console.log('collected: ' + collected);
+				console.log('total: ' + total);
+				
+				var container = $('#mounts-row > .mounts-container');
+				
+				$.each(mounts, function(k,v) {
+					console.log(mounts[k]);
+					
+					var mountToAdd = $('<div class="mount"><div class="mount-header"><h3 class="mount-name">' + mounts[k]['name'] + '</h3></div><div class="mount-body"><p>Placeholder</p></div></div>');
+					mountToAdd.appendTo(container);
+				});
+
 				$('#search form').css('opacity', '1');
 			}
 		});

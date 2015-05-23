7f9aaf2baf6a758816c58a1ae1996b7fe503bf75
mariocatch <mariocatch@gmail.com>
2015-05-23 17:28:54 -0400

    Add dynamic character data (name, class, thumbnail portrait), aesthetic fades to new requests, removing old data on new requests, 100vh on results section so window scrolling doesn't annoy the user.

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
+	
+	var classes = {
+		0: 'N/A',
+		1: 'Warrior',
+		2: 'Paladin',
+		3: 'Hunter',
+		4: 'Rogue',
+		5: 'Priest',
+		6: 'DeathKnight',
+		7: 'Shaman',
+		8: 'Mage',
+		9: 'Warlock',
+		10: 'Monk',
+		11: 'Druid'
+	};
+	
+	$('#btn-get-started').click(function(e) {
+		e.preventDefault();
+		$('#character').focus();
+	});
+	
 	$('#search form').submit(function(e) {
 		e.preventDefault();
+		$('#results .container').fadeOut();
+		var container = $('#mounts-row > .mounts-container');
+		container.empty();
 		
 		$('#search form').css('opacity', '0.5');
 		
@@ -10,6 +34,7 @@ $(document).ready(function() {
 		
 		var rootApi = 'http://us.battle.net/api/wow/';
 		var mountsApi = rootApi + 'character/' + server + '/' + character + '?locale=en_US&fields=mounts';
+		var thumbnailApi = 'http://us.battle.net/static-render/us/';
 		
 		// Example: http://us.battle.net/api/wow/character/stormrage/vesie?locale=en_US&fields=mounts
 		
@@ -21,7 +46,7 @@ $(document).ready(function() {
 		 	jsonpCallback: 'callback',
             dataType: 'jsonp', 
 			jsonp: 'jsonp',
-			success: function(data) {
+			success: function(data) {				
 				/*
 					<div class="mount">
 						<div class="mount-header">
@@ -36,20 +61,21 @@ $(document).ready(function() {
 				var collected = data.mounts.numCollected;
 				var total = data.mounts.numCollected + data.mounts.numNotCollected;
 				var mounts = data.mounts.collected;
+				thumbnailApi = thumbnailApi + data.thumbnail;
 				
-				console.log('collected: ' + collected);
-				console.log('total: ' + total);
+				$('#results .character-name').html(data.name);
+				$('#results .character-class').html(classes[data.class]);
+				$('#results .character-mounts-collected').html(collected);
+				$('#results .total-mounts').html(total);				
+				$('#character-thumbnail').attr('src', thumbnailApi);
 				
-				var container = $('#mounts-row > .mounts-container');
-				
-				$.each(mounts, function(k,v) {
-					console.log(mounts[k]);
-					
+				$.each(mounts, function(k,v) {					
 					var mountToAdd = $('<div class="mount"><div class="mount-header"><h3 class="mount-name">' + mounts[k]['name'] + '</h3></div><div class="mount-body"><p>Placeholder</p></div></div>');
 					mountToAdd.appendTo(container);
 				});
 
 				$('#search form').css('opacity', '1');
+				$('#results .container').fadeIn();
 			}
 		});
 		
